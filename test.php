8a25218f56b9560018891da0f8cfe220dc3de9c0
fc-prashantsaini <92972358+fc-prashantsaini@users.noreply.github.com>
2024-06-03 17:13:32 +0530

    Update test.php

--- test.php
+++ test.php
@@ -1,4 +1,10 @@
 <?php
 // remote.php
-echo "Remote file inclusion successful!";
+
+if (isset($_GET['action']) && $_GET['action'] === 'download') {
+    echo "Remote file inclusion successful! Download action triggered.";
+    // You can add additional logic here, such as serving a file
+} else {
+    echo "Remote file inclusion successful! No specific action triggered.";
+}
 ?>
